c90703bdb9cb7fa7c019b18aaae523750132e4e1
Aman Raj <amanpie9@gmail.com>
2021-06-29 10:01:58 +0530

    styled the two pages

--- lib/list.dart
+++ lib/list.dart
@@ -22,16 +22,49 @@ class _TaskState extends State<Task> {
         padding: EdgeInsets.all(5),
         margin: EdgeInsets.all(5),
         height: 100,
-        color: Colors.lightGreen,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
         child: Center(
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              SizedBox(
+                width: 5,
+              ),
               CircleAvatar(
                   backgroundImage: NetworkImage(
                       'https://lh3.googleusercontent.com/a-/AAuE7mChgTiAe-N8ibcM3fB_qvGdl2vQ9jvjYv0iOOjB=s96-c'),
                   radius: 30.0),
-              Text("Java", style: TextStyle(fontSize: 30)),
+              SizedBox(
+                width: 100,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Java", style: TextStyle(fontSize: 30)),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text('1996', style: TextStyle(fontSize: 10)),
+                ],
+              ),
+              SizedBox(
+                width: 30,
+              ),
             ],
           ),
         ),
