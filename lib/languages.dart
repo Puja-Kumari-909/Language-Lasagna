f3fe20a35aacb144d8d1d4bb61c56b55aa1cbaf7
Aman Raj <amanpie9@gmail.com>
2021-06-30 01:22:15 +0530

    IMAGE SET

--- lib/languages.dart
+++ lib/languages.dart
@@ -7,17 +7,22 @@ class Languages {
 }
 
 List<Languages> languageData = [
+  Languages('/assets/go.png', "GO", "some details about cpp", "2001"),
+  Languages('/assets/r.png', "R", "some details about cpp", "2001"),
+  Languages('/assets/kotlin.png', "KOTLIN", "some details about cpp", "2001"),
+  Languages(
+      '/assets/rubyonrails.png', "RUBY", "some details about cpp", "2001"),
+  Languages('/assets/java.png', "JAVA", "some details about cpp", "2001"),
+  Languages('/assets/dart.png', "DART", "some details about cpp", "2001"),
+  Languages('/assets/matlab.png', "MATLAB", "some details about cpp", "2001"),
+  Languages('/assets/csharp.png', "C#", "some details about cpp", "2001"),
   Languages('/assets/cpp.png', "CPP", "some details about cpp", "2001"),
-  Languages('/assets/cpp.png', "CPP", "some details about cpp", "2001"),
-  Languages('/assets/cpp.png', "CPP", "some details about cpp", "2001"),
-  Languages('/assets/cpp.png', "CPP", "some details about cpp", "2001"),
-  Languages('/assets/cpp.png', "CPP", "some details about cpp", "2001"),
-  Languages('/assets/cpp.png', "CPP", "some details about cpp", "2001"),
-  Languages('/assets/cpp.png', "CPP", "some details about cpp", "2001"),
-  Languages('/assets/cpp.png', "CPP", "some details about cpp", "2001"),
-  Languages('/assets/cpp.png', "CPP", "some details about cpp", "2001"),
-  Languages('/assets/cpp.png', "CPP", "some details about cpp", "2001"),
+  Languages('/assets/javascript.png', "JAVA-SCRIPT", "some details about cpp",
+      "2001"),
+  Languages('/assets/python.png', "PYTHON", "some details about cpp", "2001"),
 ];
+
+
 
   // const TaskTile(
   //     {this.isTrue,
